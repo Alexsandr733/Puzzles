a2640e0c09f37b9d2edca03597111b417f02d65c
alexandr733 <adetyonyshev@yandex.ru>
2017-04-18 14:28:25 +0300

    test1428

--- _include/js/script.js
+++ _include/js/script.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
   // При загрузке страницы выполнится весь написанный здесь код
-   alert("Привет!!!");
 
 
 
+var images = '0_0.png';
+var imagesurl = '_include/img/'+images;
+alert(imagesurl);
+var pozition;
+pozition=0;
+for (var i=1; i<=9;i++){
+  pozition+=140;
+
+  $('.elements').append('<div class="picture"></div>').css('top',pozition)
+  // .css( "background-image", "url('"+imagesurl+"')");
+.css( "background-image", "url('.._include/img/0_0.png')");
+
+}
 
 
 });
