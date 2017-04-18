f88f53d989f23adfb40899ae7529a7cca5373dab
alexandr733 <adetyonyshev@yandex.ru>
2017-04-18 18:36:18 +0300

    errors1836

--- _include/js/script.js
+++ _include/js/script.js
@@ -9,13 +9,11 @@
     var pozitions = {
     };
 
-    function imagesCreate () {
+    var pictureNames = [];
+    var wrongMass = [];
+    var images = '';
 
-      var pictureNames = [];
-      var pozition;
-      pozition=565;
-      var images = '';
-      var imagesurl = '';
+    function fillingMass(){
       for (var i=0; i<=8;i++){
 
         // считаем номер картинки
@@ -35,10 +33,52 @@
 
         var pushed = pictureNames.push(images);
 
+      }
+    }
+
+    function getWrongMass(){
+      var rand;
+      var checkMass = [];
+      for(var i=0; i<=8;i++){
+
+        while (var fl == 0) {
+       rand = getRandom();
+          for (var j = 0; j < checkMass.length; j++) {
+              if (checkMass[j] === rand) {
+                fl = 1;
+                  return fl;
+              }
+          }
+        }
+        wrongMass[rand] = pictureNames[i];
+      }
+    }
+
+
+    function imagesCreate () {
+      getWrongMass();
+      var pozition=565;
+      var imagesurl = '';
+
+      for (var i=0; i<=8;i++){
+
+        // считаем номер картинки
+        /*
+        var iter=0;
+        if(i>=0 && i<=2){
+          images = '0_'+i;
+        }
+        if(i>=3 && i<=5){
+          iter=i-3;
+          images='1_'+iter;
+        }
+        if(i>=6 && i<=8){
+          iter=i-6;
+          images='2_'+iter;
+        }
         var r = getRandom();
-        console.log(r, pictureNames[r]);
-      //  alert(r);
-      //  alert(pictureNames[r]);
+        console.log(r, pictureNames[r]);*/
+        var dop = wrongMass[i];
 
         // меняем позицию div и задаём фон
         if (i==3 || i==6){
@@ -51,7 +91,7 @@
         if(i>5){
           $(".picture").eq(i).css('top',270);//690
           }
-          imagesurl = '_include/img/'+images+'.png';
+          imagesurl = '_include/img/'+dop+'.png';
           $(".picture").eq(i).css('left',pozition).css( "background-image", "url('"+imagesurl+"')");
           //.css( "background-image", "url('_include/img/0_0.png')");
           // .css( "background-image", "url('"+imagesurl+"')");
@@ -62,6 +102,7 @@
 
     }
 
+     fillingMass();
     imagesCreate();
 
   });
