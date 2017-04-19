cfab7fdebb56c84f80a06206b05fc19ab638cc5a
alexandr733 <adetyonyshev@yandex.ru>
2017-04-19 18:11:01 +0300

    error1810

--- _include/js/script.js
+++ _include/js/script.js
@@ -1,9 +1,9 @@
   $(document).ready(function() {
   //   // При загрузке страницы выполнится весь написанный здесь код
-  var check = [];
+  //var check = [];
   var data = {
     button:$('.ready'),
-    checkMass:check
+    checkMass:check =[]
   };
 
 
@@ -30,21 +30,25 @@
   function randomPoz () {
     var r=getRandom();
     var fl=0;
+    var j;
     if(data.checkMass.length === 0){
       data.checkMass[0]=r;
     }
     while (fl === 0) {
       r = getRandom();
-      for (var j = 0; j < data.checkMass.length; j++) {
+      for (j = 0; j < data.checkMass.length; j++) {
         if (data.checkMass[j] === r) {
           fl++;
-          data.checkMass[j]=r;
           return fl;
         }
       }
     }
     alert(data.checkMass[0]+'its 0');
+    data.checkMass[j]=r;
     return r;
   }
   randomPoz ();
+
+
+
 });
