26910bc91c5811c662f31db683eaa08bcea61a93
alexandr733 <adetyonyshev@yandex.ru>
2017-04-20 12:39:48 +0300

    errors1239

--- _include/js/main.js
+++ _include/js/main.js
@@ -1,4 +1,44 @@
 
-  var data = {
-    button:$('.ready'),
-  };
+function getRandom(min, max){
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function detect (obj){
+  var png = [];
+  var cell = [];
+  for( var i = 1; i <= 9; i++){
+    var massi = i-1;
+    png[massi] =   $(".picture").eq(massi);
+    cell[massi] =  $(".pazzl").eq(massi);
+  }
+  obj.picture = png;
+  obj.pazzle = cell;
+}
+
+function randomPoz (obj) {
+  var imagesclass = '';
+  var images = '';
+  for (var i=0; i<=8;i++){
+    var randX = getRandom(510, 770);
+    var randY = getRandom(10, 270);
+    // генерируем имя класса картинки
+    var iter=0;
+    if(i>=0 && i<=2){
+      images = '0-'+i;
+    }
+    if(i>=3 && i<=5){
+      iter=i-3;
+      images='1-'+iter;
+    }
+    if(i>=6 && i<=8){
+      iter=i-6;
+      images='2-'+iter;
+    }
+    imagesclass = 'pic'+images;
+    console.log(imagesclass);
+
+    obj.picture[i].addClass(imagesclass);
+    obj.picture[i].css({left: randX, top: randY});
+    //.css( "background-image", "url('"+imagesurl+"')")
+  }
+}
--- _include/js/script.js
+++ _include/js/script.js
@@ -1,47 +1,10 @@
   $(document).ready(function() {
-  // При загрузке страницы выполнится весь написанный здесь код
+    // При загрузке страницы выполнится весь написанный здесь код
+    var data = {
+      button:$('.ready'),
+    };
 
-  function getRandom(min, max){
-    return Math.round(Math.random() * (max - min) + min);
-  }
+    detect (data);
+    randomPoz (data);
 
-  function detect (obj){
-    var png = [];
-    var cell = [];
-    for( var i = 1; i <= 9; i++){
-      var massi = i-1;
-      png[massi] =   $(".picture").eq(massi);
-      cell[massi] =  $(".pazzl").eq(massi);
-    }
-    obj.picture = png;
-    obj.pazzle = cell;
-  }
-  detect (data);
-
-  function randomPoz (obj) {
-    var imagesurl = '';
-    var images = '';
-    for (var i=0; i<=8;i++){
-      var randX = getRandom(510, 770);
-      var randY = getRandom(10, 270);
-      // генерируем имя картинки
-      var iter=0;
-      if(i>=0 && i<=2){
-        images = '0_'+i;
-      }
-      if(i>=3 && i<=5){
-        iter=i-3;
-        images='1_'+iter;
-      }
-      if(i>=6 && i<=8){
-        iter=i-6;
-        images='2_'+iter;
-      }
-      imagesurl = '_include/img/'+images+'.png';
-      console.log(images);
-      obj.picture[i].css({left: randX, top: randY}).css( "background-image", "url('"+imagesurl+"')");
-    }
-  }
-randomPoz (data);
-
-});
+  });
