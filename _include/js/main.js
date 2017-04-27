ff44fca00394c243c5bde42057dd036ce48d6b5b
alexandr733 <adetyonyshev@yandex.ru>
2017-04-27 13:01:10 +0300

    control1301

--- _include/js/main.js
+++ _include/js/main.js
@@ -9,14 +9,14 @@
     var cell = [];
     for( var i = 0; i < 9; i++){
 
-    png[i] = obj.content.find(".picture").eq(i);
-    png[i].data({pos: png[i].position()});
-    cell[i] = obj.content.find(".pazzl").eq(i);
+      png[i] = obj.content.find(".picture").eq(i);
+      png[i].data({pos: png[i].position()});
+      cell[i] = obj.content.find(".pazzl").eq(i);
 
     }
 
-    obj.picture = png;
-    obj.pazzle = cell;
+  obj.picture = png;
+  obj.pazzle = cell;
 
   }
 
@@ -26,31 +26,22 @@
     for (var i=0; i<=2; i++){
       for (var j=0; j<=2; j++){
 
-        //var randX = getRandom(510, 770);
-        //var randY = getRandom(10, 270);
         var pos = {left: getRandom(510, 770), top: getRandom(10, 270)};
 
         obj.picture[index].addClass('pic' + i + "-" + j);
-        // obj.picture[index].css({left: randX, top: randY});
-        // obj.picture[index].data({pos: obj.picture[index].position()});
         obj.picture[index].css(pos);
         obj.picture[index].data({pos: pos});
-
-        //console.log(allPicture[index]);
 
         index++;
       }
     }
   }
 
-
-
   // начинаем двигать
   function drag (obj){
 
     var parents = obj.content;
     var elems = obj.allPicture;
-    //var elems = obj.picture;
     var doc  = obj.doc;
 
     // События нажатия
@@ -79,8 +70,8 @@
 
         // Новая позиция элемента
         pos.new_pos = {
-          left: pos.cursor.left - pos.parents.left - pos.inner.left,
-          top: pos.cursor.top - pos.parents.top - pos.inner.top
+          top: pos.cursor.top - pos.parents.top - pos.inner.top,
+          left: pos.cursor.left - pos.parents.left - pos.inner.left
         };
 
         // Ограничение перемещения элемента
@@ -109,56 +100,44 @@
 
         doc.on('mouseup', function() {
 
-        var trigger = 0;
-
-
-        pos.new_pos = elem.data().pos;
-
-        for (var coorY=12; coorY<=252; coorY+=120){
-          for (var coorX=42; coorX<=282; coorX+=120){
-
-          // находим крайние точки ячеек
-          var coorRX = coorX + 124;
-          var coorRY = coorY + 124;
-
         //  pos.new_pos = elem.data().pos;
+          console.log(elem.data().pos);
+          console.log(obj.picture[1].data().pos.top);
+          //console.log(obj.picture[1].position());
 
-          // проверяем попадает ли центр элемента внутрь ячейки
-          if (pos.centerX>coorX && pos.centerX < coorRX && pos.centerY>coorY && pos.centerY < coorRY) {
+          for (var coorY = 12; coorY <= 252; coorY += 120){
+            for (var coorX = 42; coorX <= 282; coorX += 120){
+
+              // находим крайние точки ячеек
+              var coorRX = coorX + 124;
+              var coorRY = coorY + 124;
+
+              // проверяем попадает ли центр элемента внутрь ячейки
+              if (pos.centerX > coorX && pos.centerX < coorRX && pos.centerY > coorY && pos.centerY < coorRY) {
+
+                // задаём в качестве новых координатов элемента координаты ячейки
+                pos.new_pos.top = coorY;
+                pos.new_pos.left = coorX;
+
+                // проверка на повторную ячейку
+                for (var i = 0; i < 9; i++) {
+
+                  if (obj.picture[i].position().left == coorX && obj.picture[i].position().top == coorY) {
 
 
+                    //pos.new_pos = elem.data().pos;
 
-            // задаём в качестве новых координатов элемента координаты ячейки
-            trigger++;
+                     pos.new_pos.top = obj.picture[i].data().pos.top;
+                     pos.new_pos.left = obj.picture[i].data().pos.left;
 
-            // проверка на повторную ячейку
-            //if (obj.iter != 0){
-            console.log('111111111111111111');
-              for (var i = 0; i < 9; i++) {
-                console.log(obj.picture[i].position());
-                console.log(coorX, coorY);
-
-                if (obj.picture[i].position().left != coorX && obj.picture[i].position().top != coorY) {
-
-                  console.log('за');
-                  pos.new_pos.top = coorY;
-                  pos.new_pos.left = coorX;
-                  //pos.new_pos = elem.data().pos;
-                  break;
+                    //pos.new_pos = obj.picture[i].data.pos;
+                    break;
                   }
                 }
-              //  }
-            break;
+                break;
+              }
             }
           }
-        }
-
-        // перемешение если mouseup сработала не над сеткой
-        if (trigger == 0) {
-
-        //  pos.new_pos = elem.data().pos;
-
-        }
 
           elem.animate({left: pos.new_pos.left, top: pos.new_pos.top}, 100, function() {
             console.log("Success");
@@ -170,7 +149,7 @@
         });
       });
 
-  doc.on('mouseup', function() {
+      doc.on('mouseup', function() {
 
       // Снять события перетаскивания и отжатия мыши
       doc.off("mouseup");
--- _include/js/script.js
+++ _include/js/script.js
@@ -1,17 +1,17 @@
-  $(document).ready(function() {
-    // При загрузке страницы выполнится весь написанный здесь код
-    var data = {
-      button:$('.ready'),
-      content:$(".content"),
-      allPicture:$(".picture"),
-      container:$(".container"),
-      doc:$(document),
-      iter:0
-    };
+$(document).ready(function() {
+  // При загрузке страницы выполнится весь написанный здесь код
+  var data = {
+    button:$('.ready'),
+    content:$(".content"),
+    allPicture:$(".picture"),
+    container:$(".container"),
+    doc:$(document),
+    iter:0
+  };
 
-    detect (data);
-    randomPoz (data);
-    drag (data);
+  detect (data);
+  randomPoz (data);
+  drag (data);
 
 
-  });
+});
